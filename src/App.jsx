2ea0ee7faaeef5354bf0e089fed5cfa5e5ffe055
xgjimmar945-afk <xgjimmar945@ieshnosmachado.org>
2025-12-23 12:45:28 +0100

    Creacion del router el path home y el path de alta de clubs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
+// Importacion de componentes
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router";
+import Inicio from "./components/Inicio";
+import AltaClub from "./components/AltaClub";
 
+// Importacion de pages 
+import Home from "./pages/Home";
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      Component: Home,
+      children: [
+        // Todo esto se ve en el Outlet
+        { index: true, Component: Inicio }, // Esto se ve en la ruta padre
+        /* {
+          path: "/directors",
+          element: <ListadoDirectores/>,
+        }, */
+        {
+          path: "/clubs/new",
+          element: <AltaClub/>,
+        },
+        {
+          path: "/clubs",
+          element: <ListadoClubes/>,
+        },
+        /* {
+          path: "/movies",
+          element: <h1>Listado de peliculas</h1>,
+        },
+        {
+          path: "/movies/new",
+          element: <h1>Alta de peliculas</h1>,
+        }, */ 
+      ],
+    },
+  ]);
+
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
   )
 }
